8ce95007a12ad5cd5cda8f1a421a4a08afadddee
Администратор <@DESKTOP-HUC7BS4>
2022-01-16 01:13:34 +0500

    Add project files.

---
+++ Dori.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Topshiriq_file_Json_API
+{
+    class Dori
+    {
+        public int id { get; set; }
+        public string dori_nomi { get; set; }
+        public string Ishlab_chiq_firma_nomi { get; set; }         
+        public decimal price { get; set; }
+        public DateTime ishlab_chiqarilgan_vaqti { get; set; }
+        public string  dori_yaroqligi{ get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _1_Topshiriq_file_Json_API
+{     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               List<string> dori = new List<string>();
+               string path = "Dorixona.txt";
+                    Console.Write("Yangi dori qushish menyusiga utish uchun 1 ni bosing :\n" +
+                        "Dorini qidirish menyusiga utish uchun 2 ni bosing :\n" +
+                        "Dorini o'chirish menyusiga utish uchun 3 raqamni bosing :\n" +
+                        "Dori ma'lumotlarini o'zgartirish menyusiga utish uchun 4 raqamni bosing :\n" +
+                        "Dori haqidagi umumiy ma'lumot olish uchun 5 raqamni bosing:\n");
+                      int raqam = int.Parse(Console.ReadLine());
+          
+
+        }
+    }
+}
